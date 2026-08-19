7980408cc4b02ab1f6b6da8cdd3058255d9734c0
Tomas Abatedaga Biole <abatedagatomas@gmail.com>
2026-08-19 20:04:57 -0300

    ABM propietarios completo con vista y formulario

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
+using InmobiliariaGrupoNN.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IRepositorioPropietario, RepositorioPropietario>();
 
 var app = builder.Build();
 
---
+++ Repositories/IRepositorioPropietario.cs
@@ -0,0 +1,14 @@
+using InmobiliariaGrupoNN.Models;
+using System.Collections.Generic;
+
+namespace InmobiliariaGrupoNN.Repositories
+{
+    public interface IRepositorioPropietario
+    {
+        IList<Propietario> ObtenerTodos();
+        Propietario ObtenerPorId(int id);
+        int Alta(Propietario propietario);
+        int Modificacion(Propietario propietario);
+        int Baja(int id);
+    }
+}
